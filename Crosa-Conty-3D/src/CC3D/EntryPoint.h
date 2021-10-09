3093115e165fb83785f7b550f55225fa2110b7c9
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-10-09 23:21:23 +0800

    add macro definition of log system

--- Crosa-Conty-3D/src/CC3D/EntryPoint.h
+++ Crosa-Conty-3D/src/CC3D/EntryPoint.h
@@ -7,8 +7,10 @@ extern CC3D::Application* CC3D::CreateApplication();
 int main(int argc, char** argv)
 {
 	CC3D::Log::Init();
-	CC3D::Log::GetCoreLogger()->warn("Initialized Log");
-	CC3D::Log::GetClientlogger()->info("Hello Crosa Conty 3D!");
+	CC3D_CORE_WARN("Initialized Log");
+	CC3D_INFO("Hello Crosa Conty 3D!");
+	int a = 5;
+	CC3D_INFO("Var = {0}", a);
 
 	auto app = CC3D::CreateApplication();
 	app->Run();
