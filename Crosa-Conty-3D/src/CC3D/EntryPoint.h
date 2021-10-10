604803612171c2a6a526454342fb49761ac7dfd6
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-10-10 11:05:37 +0800

    Revert "add macro definition of log system"
    This reverts commit e8923f8b5fa3ac956050597ad66677d01ad8425c.

--- Crosa-Conty-3D/src/CC3D/EntryPoint.h
+++ Crosa-Conty-3D/src/CC3D/EntryPoint.h
@@ -7,10 +7,8 @@ extern CC3D::Application* CC3D::CreateApplication();
 int main(int argc, char** argv)
 {
 	CC3D::Log::Init();
-	CC3D_CORE_WARN("Initialized Log");
-	CC3D_INFO("Hello Crosa Conty 3D!");
-	int a = 5;
-	CC3D_INFO("Var = {0}", a);
+	CC3D::Log::GetCoreLogger()->warn("Initialized Log");
+	CC3D::Log::GetClientlogger()->info("Hello Crosa Conty 3D!");
 
 	auto app = CC3D::CreateApplication();
 	app->Run();
--- Crosa-Conty-3D/src/CC3D/Log.h
+++ Crosa-Conty-3D/src/CC3D/Log.h
@@ -13,23 +13,11 @@ namespace CC3D {
 		static void Init();
 
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientlogger() { return s_ClientLogger; }
 	private:
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
 	};
 }
 
-// core log macro
-#define CC3D_CORE_TRACE(...)		::CC3D::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define CC3D_CORE_INFO(...)			::CC3D::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define CC3D_CORE_WARN(...)			::CC3D::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define CC3D_CORE_ERROR(...)		::CC3D::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define CC3D_CORE_FATAL(...)		::CC3D::Log::GetCoreLogger()->fatal(__VA_ARGS__) 
 
-// client log macro
-#define CC3D_TRACE(...)				::CC3D::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define CC3D_INFO(...)				::CC3D::Log::GetClientLogger()->info(__VA_ARGS__)
-#define CC3D_WARN(...)				::CC3D::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define CC3D_ERROR(...)				::CC3D::Log::GetClientLogger()->error(__VA_ARGS__)
-#define CC3D_FATAL(...)				::CC3D::Log::GetClientLogger()->fatal(__VA_ARGS__) 
