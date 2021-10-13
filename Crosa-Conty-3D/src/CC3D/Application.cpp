3bee4b392fa0ad99d59e3915a8cbaf8964f9f14f
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-10-13 20:15:02 +0800

    layer->OnAttach() will call inside LayerStack

--- Crosa-Conty-3D/src/CC3D/Application.cpp
+++ Crosa-Conty-3D/src/CC3D/Application.cpp
@@ -31,13 +31,11 @@ namespace CC3D {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* layer)
 	{
 		m_LayerStack.PushOverlay(layer);
-		layer->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e)
--- Crosa-Conty-3D/src/CC3D/LayerStack.cpp
+++ Crosa-Conty-3D/src/CC3D/LayerStack.cpp
@@ -18,11 +18,13 @@ namespace CC3D {
 	void LayerStack::PushLayer(Layer* layer)
 	{
 		m_LayerInsert = m_Layers.emplace(m_LayerInsert, layer);
+		layer->OnAttach();
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay)
 	{
 		m_Layers.emplace_back(overlay);
+		overlay->OnAttach();
 	}
 
 	void LayerStack::PopLayer(Layer* layer)
