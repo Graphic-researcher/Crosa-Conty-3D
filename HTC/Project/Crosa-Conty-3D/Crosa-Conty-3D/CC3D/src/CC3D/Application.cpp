5707cef5b6f3b475986761db1c4c51cecf9104f3
logic-three-body <1552451580@qq.com>
2021-10-16 17:57:09 +0800

    update code in Lec21 Vertex buffer layout

--- HTC/Project/Crosa-Conty-3D/Crosa-Conty-3D/CC3D/src/CC3D/Application.cpp
+++ HTC/Project/Crosa-Conty-3D/Crosa-Conty-3D/CC3D/src/CC3D/Application.cpp
@@ -14,6 +14,28 @@ namespace CC3D {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case CC3D::ShaderDataType::Float:    return GL_FLOAT;
+		case CC3D::ShaderDataType::Float2:   return GL_FLOAT;
+		case CC3D::ShaderDataType::Float3:   return GL_FLOAT;
+		case CC3D::ShaderDataType::Float4:   return GL_FLOAT;
+		case CC3D::ShaderDataType::Mat3:     return GL_FLOAT;
+		case CC3D::ShaderDataType::Mat4:     return GL_FLOAT;
+		case CC3D::ShaderDataType::Int:      return GL_INT;
+		case CC3D::ShaderDataType::Int2:     return GL_INT;
+		case CC3D::ShaderDataType::Int3:     return GL_INT;
+		case CC3D::ShaderDataType::Int4:     return GL_INT;
+		case CC3D::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		CC3D_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
+
 
 	Application::Application()
 	{
@@ -32,18 +54,44 @@ namespace CC3D {
 		glBindVertexArray(m_VertexArray);
 
 
-
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		//   COORDINATES        /     COLORS		 /
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
+
+		//   COORDINATES        /     COLORS		 /
+		//float vertices1[3 * 7] = {
+		//	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+		//	 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+		//	 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f
+		//};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
 
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),///to next vertex
+				(const void*)element.Offset);///to next attribute
+			index++;
+		}
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 
@@ -51,11 +99,14 @@ namespace CC3D {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
+				v_Color = a_Color;
 				v_Position = a_Position;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
@@ -67,10 +118,12 @@ namespace CC3D {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				//color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
@@ -85,7 +138,7 @@ namespace CC3D {
 
 		while (m_Running)
 		{
-			glClearColor(1, 0, 1, 1);
+			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
 			m_Shader->Bind();
--- HTC/Project/Crosa-Conty-3D/Crosa-Conty-3D/CC3D/src/Platform/OpenGL/OpenGLBuffer.h
+++ HTC/Project/Crosa-Conty-3D/Crosa-Conty-3D/CC3D/src/Platform/OpenGL/OpenGLBuffer.h
@@ -12,8 +12,13 @@ namespace CC3D {
 
 		virtual void Bind() const;
 		virtual void Unbind() const;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
