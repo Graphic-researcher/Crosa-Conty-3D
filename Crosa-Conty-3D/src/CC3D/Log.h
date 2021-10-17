e38c63047b28caf930c8c7f30c9580df39830e04
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-10-17 22:08:45 +0800

    Fixed wrong of log.h

--- Crosa-Conty-3D/src/CC3D/Log.h
+++ Crosa-Conty-3D/src/CC3D/Log.h
@@ -23,11 +23,11 @@ namespace CC3D {
 #define CC3D_CORE_INFO(...)			::CC3D::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define CC3D_CORE_WARN(...)			::CC3D::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define CC3D_CORE_ERROR(...)		::CC3D::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define CC3D_CORE_FATAL(...)		::CC3D::Log::GetCoreLogger()->fatal(__VA_ARGS__) 
+#define CC3D_CORE_CRITICAL(...)		::CC3D::Log::GetCoreLogger()->critical(__VA_ARGS__) 
 
 // client log macro
 #define CC3D_TRACE(...)				::CC3D::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define CC3D_INFO(...)				::CC3D::Log::GetClientLogger()->info(__VA_ARGS__)
 #define CC3D_WARN(...)				::CC3D::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define CC3D_ERROR(...)				::CC3D::Log::GetClientLogger()->error(__VA_ARGS__)
-#define CC3D_FATAL(...)				::CC3D::Log::GetClientLogger()->fatal(__VA_ARGS__) 
+#define CC3D_CRITICAL(...)				::CC3D::Log::GetClientLogger()->critical(__VA_ARGS__) 
