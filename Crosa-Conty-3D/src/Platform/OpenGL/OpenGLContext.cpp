b60ba31ce7e6889532ee1294e023f3dfa6681f52
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-10-25 11:18:08 +0800

    Added check for OpenGL version

--- Crosa-Conty-3D/src/Platform/OpenGL/OpenGLContext.cpp
+++ Crosa-Conty-3D/src/Platform/OpenGL/OpenGLContext.cpp
@@ -24,6 +24,15 @@ namespace CC3D {
 		CC3D_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
 		CC3D_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 		CC3D_CORE_INFO("  GLSL_Version: {0}", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+#ifdef CC3D_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		CC3D_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 6), "CC3D requires at least OpenGL version 4.6!");
+#endif
 	}
 	void OpenGLContext::SwapBuffers()
 	{
