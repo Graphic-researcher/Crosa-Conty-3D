dc637155d26cd7b005bdb0180e2e08d9923e00cb
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-10-25 10:32:11 +0800

    Call the layer's OnDetach before deleting it

--- Crosa-Conty-3D/src/CC3D/Core/LayerStack.cpp
+++ Crosa-Conty-3D/src/CC3D/Core/LayerStack.cpp
@@ -10,7 +10,10 @@ namespace CC3D {
 	LayerStack::~LayerStack()
 	{
 		for (Layer* layer : m_Layers)
+		{
+			layer->OnDetach();
 			delete layer;
+		}	
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
