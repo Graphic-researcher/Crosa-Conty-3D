e6e62725e23cea5904abf88a60bea00029f8bbe8
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-10-31 12:10:21 +0800

    Added file reading check

--- Crosa-Conty-3D/src/Platform/OpenGL/OpenGLShader.cpp
+++ Crosa-Conty-3D/src/Platform/OpenGL/OpenGLShader.cpp
@@ -133,10 +133,18 @@ namespace CC3D {
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(in.tellg());
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], result.size());
+				in.close();
+			}
+			else
+			{
+				CC3D_CORE_ERROR("Could not read from file '{0}'", filepath);
+			}
 		}
 		else
 		{
