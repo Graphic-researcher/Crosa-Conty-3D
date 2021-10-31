e89f9cfe7750bb4dfc90bcf58cddd3e074f6b3c5
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-10-31 14:30:07 +0800

    Fixed bug that windowswindow will create 2 glfw window

--- Crosa-Conty-3D/src/Platform/Windows/WindowsWindow.cpp
+++ Crosa-Conty-3D/src/Platform/Windows/WindowsWindow.cpp
@@ -93,8 +93,6 @@ namespace CC3D
 			++s_GLFWWindowCount;
 		}
 		
-		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		
 		m_Context = GraphicsContext::Create(m_Window);
 		m_Context->Init();
 
