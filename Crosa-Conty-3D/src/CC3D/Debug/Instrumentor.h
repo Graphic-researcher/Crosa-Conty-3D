4dab1ad47ecc929000857e4fdfdbce2fa0a53998
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-11-05 18:49:17 +0800

    Optimization in Instrumentor.h
    Former-commit-id: de62d2dfab87b12152a91f23c02cc5042835fca0

--- Crosa-Conty-3D/src/CC3D/Debug/Instrumentor.h
+++ Crosa-Conty-3D/src/CC3D/Debug/Instrumentor.h
@@ -83,14 +83,11 @@ namespace CC3D {
 		{
 			std::stringstream json;
 
-			std::string name = result.Name;
-			std::replace(name.begin(), name.end(), '"', '\'');
-
 			json << std::setprecision(3) << std::fixed;
 			json << ",{";
 			json << "\"cat\":\"function\",";
 			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
-			json << "\"name\":\"" << name << "\",";
+			json << "\"name\":\"" << result.Name << "\",";
 			json << "\"ph\":\"X\",";
 			json << "\"pid\":0,";
 			json << "\"tid\":" << result.ThreadID << ",";
@@ -98,7 +95,8 @@ namespace CC3D {
 			json << "}";
 
 			std::lock_guard lock(m_Mutex);
-			if (m_CurrentSession) {
+			if (m_CurrentSession) 
+			{
 				m_OutputStream << json.str();
 				m_OutputStream.flush();
 			}
@@ -125,8 +123,10 @@ namespace CC3D {
 
 		// Note: you must already own lock on m_Mutex before
 		// calling InternalEndSession()
-		void InternalEndSession() {
-			if (m_CurrentSession) {
+		void InternalEndSession() 
+		{
+			if (m_CurrentSession) 
+			{
 				WriteFooter();
 				m_OutputStream.close();
 				delete m_CurrentSession;
@@ -166,14 +166,65 @@ namespace CC3D {
 		std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
 		bool m_Stopped;
 	};
+
+	namespace InstrumentorUtils {
+
+		template <size_t N>
+		struct ChangeResult
+		{
+			char Data[N];
+	};
+
+		template <size_t N, size_t K>
+		constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+		{
+			ChangeResult<N> result = {};
+
+			size_t srcIndex = 0;
+			size_t dstIndex = 0;
+			while (srcIndex < N)
+			{
+				size_t matchIndex = 0;
+				while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+					matchIndex++;
+				if (matchIndex == K - 1)
+					srcIndex += matchIndex;
+				result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+				srcIndex++;
+			}
+			return result;
+		}
+	}
 }
 
-#define CC3D_PROFILE 0
+#define CC3D_PROFILE 1
 #if CC3D_PROFILE
+// Resolve which function signature macro will be used. Note that this only
+// is resolved when the (pre)compiler starts, so the syntax highlighting
+// could mark the wrong one in your editor!
+#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+#define CC3D_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__DMC__) && (__DMC__ >= 0x810)
+#define CC3D_FUNC_SIG __PRETTY_FUNCTION__
+#elif (defined(__FUNCSIG__) || (_MSC_VER))
+#define CC3D_FUNC_SIG __FUNCSIG__
+#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+#define CC3D_FUNC_SIG __FUNCTION__
+#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+#define CC3D_FUNC_SIG __FUNC__
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+#define CC3D_FUNC_SIG __func__
+#elif defined(__cplusplus) && (__cplusplus >= 201103)
+#define CC3D_FUNC_SIG __func__
+#else
+#define CC3D_FUNC_SIG "CC3D_FUNC_SIG unknown!"
+#endif
+
 #define CC3D_PROFILE_BEGIN_SESSION(name, filepath) ::CC3D::Instrumentor::Get().BeginSession(name, filepath)
 #define CC3D_PROFILE_END_SESSION() ::CC3D::Instrumentor::Get().EndSession()
-#define CC3D_PROFILE_SCOPE(name) ::CC3D::InstrumentationTimer timer##__LINE__(name);
-#define CC3D_PROFILE_FUNCTION() CC3D_PROFILE_SCOPE(__FUNCSIG__)
+#define CC3D_PROFILE_SCOPE(name) constexpr auto fixedName = ::CC3D::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+									::CC3D::InstrumentationTimer timer##__LINE__(fixedName.Data)
+#define CC3D_PROFILE_FUNCTION() CC3D_PROFILE_SCOPE(CC3D_FUNC_SIG)
 #else
 #define CC3D_PROFILE_BEGIN_SESSION(name, filepath)
 #define CC3D_PROFILE_END_SESSION()
