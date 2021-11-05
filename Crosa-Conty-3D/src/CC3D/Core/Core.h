0e5d56bb952f3629632c51ab70c8b183f34276dc
darkwuta <57851197+darkwuta@users.noreply.github.com>
2021-11-05 18:19:07 +0800

    Fixed IndexCount selection in OpenGLRendererAPI::DrawIndexed
    Former-commit-id: 9c2bca96343c2be9c7bffcadc5930bdc54ecbd57

--- Crosa-Conty-3D/src/CC3D/Core/Core.h
+++ Crosa-Conty-3D/src/CC3D/Core/Core.h
@@ -44,9 +44,23 @@
 #error "Unknown platform!"
 #endif // End of platform detection
 
+#ifdef CC3D_DEBUG
+#if defined(CC3D_PLATFORM_WINDOWS)
+#define CC3D_DEBUGBREAK() __debugbreak()
+#elif defined(CC3D_PLATFORM_LINUX)
+#include <signal.h>
+#define CC3D_DEBUGBREAK() raise(SIGTRAP)
+#else
+#error "Platform doesn't support debugbreak yet!"
+#endif
+#define CC3D_ENABLE_ASSERTS
+#else
+#define CC3D_DEBUGBREAK()
+#endif
+
 #ifdef CC3D_ENABLE_ASSERTS
-		#define CC3D_ASSERT(x, ...) {if(!(x)){CC3D_ERROR("Assertion Failed: {0}", __VA_ARGS__);__debugbreak();} }
-		#define CC3D_CORE_ASSERT(x, ...){if(!(x)) {CC3D_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__);__debugbreak;}}
+#define CC3D_ASSERT(x, ...) { if(!(x)) { CC3D_ERROR("Assertion Failed: {0}", __VA_ARGS__); CC3D_DEBUGBREAK(); } }
+#define CC3D_CORE_ASSERT(x, ...) { if(!(x)) { CC3D_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); CC3D_DEBUGBREAK(); } }
 #else
 		#define	CC3D_ASSERT(x, ...) 
 		#define CC3D_CORE_ASSERT(x, ...) 
--- Crosa-Conty-3D/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Crosa-Conty-3D/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -61,7 +61,7 @@ namespace CC3D {
 
 	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount)
 	{
-		uint32_t count = indexCount ? vertexArray->GetIndexBuffer()->GetCount() : indexCount;
+		uint32_t count = indexCount ? indexCount : vertexArray->GetIndexBuffer()->GetCount();// TODO 添加indexCount = 0 时的情况
 		glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
