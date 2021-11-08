c38d29cdd9ab6c0f1e3355e74e2ae6d4892c32db
logic-three-body <1552451580@qq.com>
2021-11-08 22:59:55 +0800

    Update code in Lec55 Scene Viewport

--- HTC/Project/Crosa-Conty-3D/Crosa-Conty-3D/CC3D/src/CC3D/Renderer/OrthographicCameraController.cpp
+++ HTC/Project/Crosa-Conty-3D/Crosa-Conty-3D/CC3D/src/CC3D/Renderer/OrthographicCameraController.cpp
@@ -64,6 +64,12 @@ namespace CC3D {
 		dispatcher.Dispatch<WindowResizeEvent>(CC3D_BIND_EVENT_FN(OrthographicCameraController::OnWindowResized));
 	}
 
+	void OrthographicCameraController::OnResize(float width, float height)
+	{
+		m_AspectRatio = width / height;
+		m_Camera.SetProjection(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+	}
+
 	bool OrthographicCameraController::OnMouseScrolled(MouseScrolledEvent& e)
 	{
 		CC3D_PROFILE_FUNCTION();
--- HTC/Project/Crosa-Conty-3D/Crosa-Conty-3D/CC3D/src/Platform/OpenGL/OpenGLFramebuffer.cpp
+++ HTC/Project/Crosa-Conty-3D/Crosa-Conty-3D/CC3D/src/Platform/OpenGL/OpenGLFramebuffer.cpp
@@ -14,10 +14,19 @@ namespace CC3D {
 	OpenGLFramebuffer::~OpenGLFramebuffer()
 	{
 		glDeleteFramebuffers(1, &m_RendererID);
+		glDeleteTextures(1, &m_ColorAttachment);
+		glDeleteTextures(1, &m_DepthAttachment);
 	}
 
 	void OpenGLFramebuffer::Invalidate()
 	{
+		if (m_RendererID)
+		{
+			glDeleteFramebuffers(1, &m_RendererID);
+			glDeleteTextures(1, &m_ColorAttachment);
+			glDeleteTextures(1, &m_DepthAttachment);
+		}
+
 		glCreateFramebuffers(1, &m_RendererID);
 		glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
 		
@@ -32,8 +41,6 @@ namespace CC3D {
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_DepthAttachment);
 		glBindTexture(GL_TEXTURE_2D, m_DepthAttachment);
 		glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height);
-		// glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height, 0,
-		// 	GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_DepthAttachment, 0);
 
 		CC3D_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is incomplete!");
@@ -44,6 +51,7 @@ namespace CC3D {
 	void OpenGLFramebuffer::Bind()
 	{
 		glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
+		glViewport(0, 0, m_Specification.Width, m_Specification.Height);
 	}
 
 	void OpenGLFramebuffer::Unbind()
@@ -51,5 +59,11 @@ namespace CC3D {
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	}
 
+	void OpenGLFramebuffer::Resize(uint32_t width, uint32_t height)
+	{
+		m_Specification.Width = width;
+		m_Specification.Height = height;
 
+		Invalidate();
+	}
 }
