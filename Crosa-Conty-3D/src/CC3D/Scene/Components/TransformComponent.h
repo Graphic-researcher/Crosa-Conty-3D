90d2c9281fd873339be6582f88a0bfbcd3cb0f84
darkmon <z214971500@hotmail.com>
2021-11-17 22:28:58 +0800

    fixed rotation error, glm now support degree directly

--- Crosa-Conty-3D/src/CC3D/Scene/Components/TransformComponent.h
+++ Crosa-Conty-3D/src/CC3D/Scene/Components/TransformComponent.h
@@ -1,18 +1,29 @@
 #pragma once
 #include <glm/glm.hpp>
-
+#include <glm/gtc/matrix_transform.hpp>
 namespace CC3D {
 
 	struct TransformComponent
 	{
-		glm::mat4 Transform{ 1.0f };
+		glm::vec3 Translation = glm::vec3{ 0.0f, 0.0f, 0.0f };
+		glm::vec3 Rotation = glm::vec3{ 0.0f, 0.0f, 0.0f };
+		glm::vec3 Scale = glm::vec3{ 1.0f, 1.0f, 1.0f };
 
 		TransformComponent() = default;
 		TransformComponent(const TransformComponent&) = default;
-		TransformComponent(const glm::mat4& transform)
-			: Transform(transform) {}
+		TransformComponent(const glm::vec3& translation)
+			: Translation(translation) {}
 
-		operator glm::mat4& () { return Transform; }
-		operator const glm::mat4& () const { return Transform; }
+		glm::mat4 GetTransform() const
+		{
+			glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), Rotation.x, glm::vec3{ 1, 0, 0 })
+				* glm::rotate(glm::mat4(1.0f), Rotation.y, glm::vec3{ 0, 1, 0 })
+				* glm::rotate(glm::mat4(1.0f), Rotation.z, glm::vec3{ 0, 0, 1 });
+
+			return glm::translate(glm::mat4(1.0f), Translation)
+				* rotation
+				* glm::scale(glm::mat4(1.0f), Scale);
+		}
+
 	};
 }
--- Crosa-Conty-3D/src/CC3D/Scene/Scene.cpp
+++ Crosa-Conty-3D/src/CC3D/Scene/Scene.cpp
@@ -49,7 +49,7 @@ namespace CC3D {
 
 		// Render 2D
 		Camera* mainCamera = nullptr;
-		glm::mat4* cameraTransform = nullptr;
+		glm::mat4 cameraTransform;
 		{
 			auto view = m_Registry.view<TransformComponent, CameraComponent>();
 			for (auto entity : view)
@@ -59,7 +59,7 @@ namespace CC3D {
 				if (camera.Primary)
 				{
 					mainCamera = &camera.Camera;
-					cameraTransform = &transform.Transform;
+					cameraTransform = transform.GetTransform();
 					break;
 				}
 			}
@@ -67,14 +67,14 @@ namespace CC3D {
 
 		if (mainCamera)
 		{
-			Renderer2D::BeginScene(*mainCamera, *cameraTransform);
+			Renderer2D::BeginScene(*mainCamera, cameraTransform);
 
 			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
 			for (auto entity : group)
 			{
 				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
-				Renderer2D::DrawQuad(transform, sprite.Color);
+				Renderer2D::DrawQuad(transform.GetTransform(), sprite.Color);
 			}
 
 			// Á£×ÓÏµÍ³ 
