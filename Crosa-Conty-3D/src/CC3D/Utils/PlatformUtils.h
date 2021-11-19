ad94c93751c40e7b426c4e2255168d0db34e31b5
darkmon <z214971500@hotmail.com>
2021-11-19 19:02:26 +0800

    Change to FileDialogs' methods so that they return std::optional.

--- Crosa-Conty-3D/src/CC3D/Utils/PlatformUtils.h
+++ Crosa-Conty-3D/src/CC3D/Utils/PlatformUtils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <optional>
 
 namespace CC3D {
 
@@ -8,8 +9,8 @@ namespace CC3D {
 	{
 	public:
 		// These return empty strings if cancelled
-		static std::string OpenFile(const char* filter);
-		static std::string SaveFile(const char* filter);
+		static std::optional<std::string> OpenFile(const char* filter);
+		static std::optional<std::string> SaveFile(const char* filter);
 	};
 
 }
--- Crosa-Conty-3D/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ Crosa-Conty-3D/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -10,7 +10,7 @@
 
 namespace CC3D {
 
-	std::string FileDialogs::OpenFile(const char* filter)
+	std::optional<std::string> FileDialogs::OpenFile(const char* filter)
 	{
 		OPENFILENAMEA ofn;
 		CHAR szFile[260] = { 0 };
@@ -23,13 +23,12 @@ namespace CC3D {
 		ofn.nFilterIndex = 1;
 		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 		if (GetOpenFileNameA(&ofn) == TRUE)
-		{
 			return ofn.lpstrFile;
-		}
-		return std::string();
+
+		return std::nullopt;
 	}
 
-	std::string FileDialogs::SaveFile(const char* filter)
+	std::optional<std::string> FileDialogs::SaveFile(const char* filter)
 	{
 		OPENFILENAMEA ofn;
 		CHAR szFile[260] = { 0 };
@@ -40,16 +39,14 @@ namespace CC3D {
 		ofn.nMaxFile = sizeof(szFile);
 		ofn.lpstrFilter = filter;
 		ofn.nFilterIndex = 1;
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 
 		// Sets the default extension by extracting it from the filter
 		ofn.lpstrDefExt = strchr(filter, '\0') + 1;
 
-		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 		if (GetSaveFileNameA(&ofn) == TRUE)
-		{
 			return ofn.lpstrFile;
-		}
-		return std::string();
-	}
 
+		return std::nullopt;
+	}
 }
